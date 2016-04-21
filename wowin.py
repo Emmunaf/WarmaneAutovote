2cd97dca8aa0c10d2e27e6782b9ed96a1b7b9b35
Emanuele Munafò <ema.muna95@gmail.com>
2016-04-21 19:22:25 +0200

    Cleaned code

--- wowin.py
+++ wowin.py
@@ -32,32 +32,25 @@ def hilite(string, status = False, bold = False):
 def autovote(username, password):
     """Starts the autovote."""
     
-    #print "Molten Autovote started..."
     url_0 = 'http://www.warmane.com/account/login'
     url = 'http://www.warmane.com/account'
     # Start a new session, to preserve the cookie
-    global s  # So I can create the function points()
-    #print "Trying to do a GET request to parse csrf-token..."
+    global s
     s = requests.session()
-    # Take session from the simple Molten index page
+    # Take session and anti-csrf Token
     t = s.get(url)
-    #print t.text  # Cloudflare, need to bypass HERE TODO
     soup = BeautifulSoup(t.text)
-    # Prendo token anti CSRF
     # <meta name="csrf-token" content="ZGRiNzQ3MWE2NWI5NTc5MDQ1M2E5ZjgzYTRhMjNhZjQ=">
     token = soup.find("meta", {"name": "csrf-token"})['content']
-    #print "Il token CSRF è: ", token
-    # The login payload
+    # The login POST payload
     login_payload = {
         'userID': username,
         'userPW': password
     }
-    # The vote payload
+    # The vote POST payload
     vote_payload = {
         'collectpoints': 'true'
     }
-
-    #print "Logging in with POST request..."
     login_headers = {
         'Accept': 'application/json, text/javascript, */*; q=0.01',
         'Origin': 'http://www.warmane.com',
@@ -77,18 +70,15 @@ def autovote(username, password):
         'Referer': url,
         'Accept-Encoding': 'gzip, deflate'
     }
-
-    # response = requests.post(url_0, data=json.dumps(data), headers=headers)
     l_response = s.post(url_0, data=login_payload, headers=login_headers)
     vote_response = s.post(url, data=vote_payload, headers=vote_headers)
     if vote_response.json()['messages']['error']:
-        print hilite("Error:", False, True)
+        print hilite("Error:", False, True)  # Bold
         print hilite(vote_response.json()['messages']['error'][0])
     else:
         print "Now you have: ",
-        print hilite(vote_response.json()['messages'].get('points'), True)
+        print hilite(vote_response.json()['messages'].get('points'), True)  # Red
         print " votepoints"
-    # print "Request method: ", r.request.method
     # points()
     return 0
 
@@ -106,23 +96,16 @@ def main():
             mypacket.show_realm()
             # Open Login page to vote easily
             autovote(user, password)
-
         else:
             print "Login failed for: " + user
             
-    print "Fine votazione del giorno:", str(datetime.date.today())    
-    # user = "alexlorens"
-    # password = "lolloasd"
-    # mypacket = Wrsp(user, password, host)
-    # print mypacket.login()
-    # print mypacket.show_realm()
+    print "End of daily vote:", str(datetime.date.today())    
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 scheduler = BlockingScheduler()
 scheduler.add_job(main, 'interval', seconds=50, hours=24)
-print('Press Ctrl+C to exit')
 try:
-    scheduler.start()
+    scheduler.start()  # CTRL+C to exit
 except (KeyboardInterrupt, SystemExit):
     pass
 
