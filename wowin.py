8b96a15c3a7f08d9f6deb737b6fd12bd31bbcf45
Emanuele Munafò <ema.muna95@gmail.com>
2016-04-21 17:13:23 +0200

    Autovote working too

--- wowin.py
+++ wowin.py
@@ -4,6 +4,22 @@
 """
 
 from wrsp import Wrsp
+import sys
+import webbrowser
+
+def hilite(string, status = False, bold = False):
+    """If tty highligth the output."""
+
+    if not sys.stdout.isatty():
+        return string
+    attr = []
+    if status:
+        attr.append('32')  # Green
+    else:
+        attr.append('31')  # Red
+    if bold:
+        attr.append('1')
+    return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)
 
 # Server data
 host = "54.213.244.47"
@@ -14,8 +30,16 @@ for line in f.readlines():
     user, password = line.rstrip().split(":")
     mypacket = Wrsp(user, password, host)
     if mypacket.login():
-        print "User: " + user + "logged succesfully!"
+        print hilite("User: " + user + " logged succesfully!", True)
         mypacket.show_realm()
+        # Open Login page to vote easily
+        url = 'http://www.warmane.com/account/'
+        # Open URL in a new tab, if a browser window is already open.
+        #webbrowser.open_new_tab(url + 'login')
+        webbrowser.open_new(url + 'login')
+        print hilite("Press Enter to continue")
+        raw_input()
+
     else:
         print "Login failed for: " + user
 
@@ -24,3 +48,4 @@ for line in f.readlines():
 # mypacket = Wrsp(user, password, host)
 # print mypacket.login()
 # print mypacket.show_realm()
+
